be58ba64f4d0d5a39d049a1898979e20fae47073
ChrysanthosM <ChrysanthosM@Outlook.com>
2026-01-24 20:07:38 +0200

    commit

--- src/main/java/org/masouras/model/mssql/j2sql/ActivityRepo.java
+++
@@ -1,9 +0,0 @@
-package org.masouras.model.mssql.j2sql;
-
-import org.masouras.base.repo.datasource.RepoBaseMSSQL;
-
-public interface ActivityRepo extends RepoBaseMSSQL {
-    enum NameOfSQL {
-        INSERT,
-    }
-}
