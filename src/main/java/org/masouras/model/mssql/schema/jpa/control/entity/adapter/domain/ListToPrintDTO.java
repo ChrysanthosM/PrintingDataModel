58a8a06de04c4622c6159cd8803c19a74c1a423f
ChrysanthosM <ChrysanthosM@Outlook.com>
2026-01-30 20:45:38 +0200

    commit

--- src/main/java/org/masouras/model/mssql/schema/jpa/control/entity/adapter/domain/ListToPrintDTO.java
+++ src/main/java/org/masouras/model/mssql/schema/jpa/control/entity/adapter/domain/ListToPrintDTO.java
@@ -3,6 +3,7 @@ package org.masouras.model.mssql.schema.jpa.control.entity.adapter.domain;
 import com.vaadin.flow.component.textfield.TextField;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.masouras.model.mssql.schema.jpa.control.entity.enums.ActivityType;
 import org.masouras.model.mssql.schema.jpa.control.vaadin.FormField;
 
 @Data
@@ -11,9 +12,9 @@ public class ListToPrintDTO {
     @FormField(component = TextField.class, order = 1, label = "Record ID")
     private Long recId;
 
-    @FormField(component = TextField.class, order = 2, label = "Final Content ID")
-    private Long finalContentId;
+    @FormField(component = TextField.class, order = 2, label = "Activity Type")
+    private ActivityType activityType;
 
-    @FormField(component = TextField.class, order = 3, label = "Activity Type")
-    private String activityType;
+    @FormField(component = TextField.class, order = 3, label = "Final Content ID")
+    private Long finalContentId;
 }
--- src/main/java/org/masouras/model/mssql/schema/jpa/control/entity/adapter/mapper/ListToPrintMapper.java
+++ src/main/java/org/masouras/model/mssql/schema/jpa/control/entity/adapter/mapper/ListToPrintMapper.java
@@ -3,6 +3,8 @@ package org.masouras.model.mssql.schema.jpa.control.entity.adapter.mapper;
 import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.StringUtils;
 import org.masouras.model.mssql.schema.jpa.control.entity.adapter.domain.ListToPrintDTO;
+import org.masouras.model.mssql.schema.jpa.control.entity.enums.ActivityType;
+import org.masouras.model.mssql.schema.jpa.control.util.EnumUtil;
 import org.masouras.model.mssql.schema.qb.structure.DbField;
 
 import java.util.List;
@@ -12,12 +14,12 @@ import java.util.Objects;
 @UtilityClass
 public class ListToPrintMapper {
 
-    public static List<ListToPrintDTO> getListToPrintDTOs(List<Map<String,Object>> rows) {
+    public static List<ListToPrintDTO> getListToPrintDTOs(List<Map<String, Object>> rows) {
         return rows.parallelStream()
                 .map(row -> new ListToPrintDTO(
                         toLong(row.get(DbField.REC_ID.systemName())),
-                        toLong(row.get(DbField.FINAL_CONTENT_ID.systemName())),
-                        Objects.toString(row.get(DbField.ACTIVITY_ID.systemName()), StringUtils.EMPTY)
+                        EnumUtil.fromCode(ActivityType.class, Objects.toString(row.get(DbField.ACTIVITY_TYPE.systemName()), StringUtils.EMPTY)),
+                        toLong(row.get(DbField.FINAL_CONTENT_ID.systemName()))
                 ))
                 .toList();
     }
